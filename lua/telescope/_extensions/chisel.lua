adedc5f5ac7d233e86556472eb31d3ca2cd4ddd6
Mr.PAPI <thesnakewitcher@gmail.com>
2023-05-03 19:02:01 -0400

    ref: renaming telescope picker and finder

--- lua/telescope/_extensions/chisel.lua
+++ lua/telescope/_extensions/chisel.lua
@@ -1,6 +1,7 @@
 local has_telescope, telescope = pcall(require, "telescope")
 if not has_telescope then
 	vim.notify("chisel.nvim requires nvim-telescope/telescope.nvim")
+    return
 end
 
 
@@ -8,20 +9,20 @@ local conf = require("telescope.config").values
 local pickers = require("telescope.pickers")
 local finders = require("telescope.finders")
 local actions = require("telescope.actions")
-local action_state = require("telescope.actions.state")
+local actions_state = require("telescope.actions.state")
 local chisel = require("chisel")
 
 
 local chisel_mappings = function(prompt_bufnr, map)
     actions.select_default:replace(function()
 	    actions.close(prompt_bufnr)
-	    local selection = action_state.get_selected_entry()
+	    local selection = actions_state.get_selected_entry()
         chisel.load(selection.value.id)
     end)
     return true
 end
 
-local chisel_finder = finders.new_table({
+local chisel_sessions_finder = finders.new_table({
     results = chisel.list(),
     entry_maker = function(entry)
         return {
@@ -32,11 +33,11 @@ local chisel_finder = finders.new_table({
     end
 })
 
-local chisel_picker = function(opts)
+local chisel_sessions_picker = function(opts)
 	opts = opts or {}
 	pickers.new(opts, {
 		prompt_title = "chisel sessions",
-        finder = chisel_finder,
+        finder = chisel_sessions_finder,
 		sorter = conf.generic_sorter(opts),
 		attach_mappings = chisel_mappings,
 	})
@@ -46,6 +47,6 @@ end
 
 return telescope.register_extension({
 	exports = {
-		chisel_sessions = chisel_picker,
+		chisel_sessions = chisel_sessions_picker,
 	},
 })
