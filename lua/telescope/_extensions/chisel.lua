77e838b0a9fb8eab0572e2560e3740dce77afcf2
Mr.PAPI <thesnakewitcher@gmail.com>
2023-05-27 20:43:17 -0400

    add: telescope chisel extension

--- lua/telescope/_extensions/chisel.lua
+++ lua/telescope/_extensions/chisel.lua
@@ -7,13 +7,25 @@ end
 
 local conf = require("telescope.config").values
 local pickers = require("telescope.pickers")
+local previewers = require("telescope.previewers")
 local finders = require("telescope.finders")
 local actions = require("telescope.actions")
 local actions_state = require("telescope.actions.state")
 local chisel = require("chisel")
 
 
-local chisel_mappings = function(prompt_bufnr, map)
+local chisel_sessions_finder = finders.new_table({
+    results = chisel.list(),
+    entry_maker = function(entry)
+        return {
+            value = entry,
+            display = entry.id,
+            ordinal = entry.id,
+        }
+    end
+})
+
+local chisel_sessions_mappings = function(prompt_bufnr, map)
     actions.select_default:replace(function()
 	    actions.close(prompt_bufnr)
 	    local selection = actions_state.get_selected_entry()
@@ -22,16 +34,12 @@ local chisel_mappings = function(prompt_bufnr, map)
     return true
 end
 
-local chisel_sessions_finder = finders.new_table({
-    results = chisel.list(),
-    entry_maker = function(entry)
-        return {
-            value = entry,
-            display = entry.id,
-            ordinal = entry.date,
-        }
-    end
+local chisel_sessions_previewer = previewers.new_termopen_previewer({
+    get_command = function(entry,status)
+        return { "chisel" , "view" , entry.value.id }
+    end,
 })
+
 
 local chisel_sessions_picker = function(opts)
 	opts = opts or {}
@@ -39,14 +47,14 @@ local chisel_sessions_picker = function(opts)
 		prompt_title = "chisel sessions",
         finder = chisel_sessions_finder,
 		sorter = conf.generic_sorter(opts),
-		attach_mappings = chisel_mappings,
+		attach_mappings = chisel_sessions_mappings,
+		previewer = chisel_sessions_previewer,
 	})
 	:find()
 end
 
-
 return telescope.register_extension({
 	exports = {
-		chisel_sessions = chisel_sessions_picker,
+		chisel = chisel_sessions_picker,
 	},
 })
