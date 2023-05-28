e28097c18152fb7ab9469a5e2bb4ef23c43704dc
Mr.PAPI <thesnakewitcher@gmail.com>
2023-05-27 20:41:26 -0400

    ref: delete unneeded plugin and config modules

--- plugin/chisel.lua
+++
@@ -1,9 +0,0 @@
-if vim.fn.has("nvim-0.8.0") == 0 then
-  vim.notify("chisel requires at least nvim-0.8.0.1")
-  return
-end
-
-if vim.g.loaded_chisel == true then
-  return
-end
-vim.g.loaded_chisel = true
